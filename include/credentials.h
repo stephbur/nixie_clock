ac137cb3ddffd78ff44016f6f7d73c3b672054f5
stephbur <stephan.burkhalter@mailbox.org>
2025-04-10 22:23:43 +0200

    empty credentials

---
+++ include/credentials.h
@@ -0,0 +1,8 @@
+#ifndef CREDENTIALS_H
+#define CREDENTIALS_H
+
+// Enter your WiFi password here.
+static const char* WIFI_PASSWORD = "WIFI_PW_HERE";
+static const char* WIFI_SSID = "WIFI_SSID_HERE";
+
+#endif // CREDENTIALS_H
