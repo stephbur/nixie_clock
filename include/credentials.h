1f39adca38b3a96d2dc206f24d3b3eaa743f6884
stephbur <stephan.burkhalter@mailbox.org>
2025-05-09 20:31:49 +0200

    Sanitize WiFi credentials

--- include/credentials.h
+++ include/credentials.h
@@ -1,8 +1,8 @@
 #ifndef CREDENTIALS_H
 #define CREDENTIALS_H
 
-// Enter your WiFi password here.
-static const char* WIFI_PASSWORD = "AMeeZu5o?wifi";
-static const char* WIFI_SSID = "Perseverance";
+// Placeholder credentials. DO NOT use for real deployments.
+static const char* WIFI_SSID = "YOUR_WIFI_SSID";
+static const char* WIFI_PASSWORD = "YOUR_WIFI_PASSWORD";
 
 #endif // CREDENTIALS_H
